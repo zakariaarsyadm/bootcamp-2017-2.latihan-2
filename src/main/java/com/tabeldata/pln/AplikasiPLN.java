b5837655f28f1ef8a6089af830c0e0d73aff76f1
dimMaryanto@win10 <software.dimmaryanto@hotmail.com>
2017-10-08 20:48:22 +0700

    update template

--- src/main/java/com/tabeldata/pln/AplikasiPLN.java
+++ src/main/java/com/tabeldata/pln/AplikasiPLN.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AplikasiPLN {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AplikasiPLN.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AplikasiPLN.class, args);
+    }
 }
